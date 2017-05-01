811f75997ea3cca265a76b83dddb3b31c60fa049
唐文明 <wenming@cshome.com>
2017-05-02 07:04:37 +0800

    主题发布时间和回复时间格式化 time-ago

---
+++ src/AppBundle/Twig/TimeExtension.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace AppBundle\Twig;
+
+/**
+ * @author Wenming Tang <wenming@cshome.com>
+ */
+class TimeExtension extends \Twig_Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter('ago', [$this, 'agoFilter'])
+        ];
+    }
+
+    public function agoFilter(\DateTime $time)
+    {
+        $difference = time() - $time->format('U');
+
+        $distanceInSeconds = round(abs($difference));
+        $distanceInMinutes = round((abs($difference) / 60));
+
+        if ($distanceInMinutes < 1) {
+            if ($distanceInSeconds < 5) {
+                return '刚刚';
+            }
+
+            return '几秒前';
+        }
+
+        if ($distanceInMinutes < 60) {
+            return sprintf('%d 分钟前', $distanceInMinutes);
+        }
+
+        if ($distanceInMinutes <= 1440) {
+            return sprintf('%d 小时前', round($distanceInMinutes / 60));
+        }
+
+        return $time->format('n月j日 H:i');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'time_extension';
+    }
+}
